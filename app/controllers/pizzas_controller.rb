a8dd483006a76ce688103915d227e355f6811fbc
son189 <son.august.7334@gmail.com>
2023-03-19 00:17:31 +0300

    pizza setup done

--- app/controllers/pizzas_controller.rb
+++ app/controllers/pizzas_controller.rb
@@ -1,5 +1,8 @@
 class PizzasController < ApplicationController
-
+def index
+pizza = Pizza.all
+render json: pizza, only: [:id, :name, :ingredients] 
+end
 
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+pizza1 = Pizza.create(name: "Margherita", ingredients: "Tomato sauce, mozzarella cheese, basil")
+pizza2 = Pizza.create(name: "Pepperoni", ingredients: "Tomato sauce, mozzarella cheese, pepperoni")
+pizza3 = Pizza.create(name: "Hawaiian", ingredients: "Tomato sauce, mozzarella cheese, ham, pineapple")
+pizza4 = Pizza.create(name: "Meat Lovers", ingredients: "Tomato sauce, mozzarella cheese, pepperoni, sausage, ham, bacon")
+pizza5 = Pizza.create(name: "Vegetarian", ingredients: "Tomato sauce, mozzarella cheese, mushrooms, onions, green peppers, black olives")
