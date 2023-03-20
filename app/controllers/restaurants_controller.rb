ab9c007815e43420508dc361849137c6cfb82cce
son189 <son.august.7334@gmail.com>
2023-03-20 21:55:20 +0300

    done

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -5,20 +5,12 @@ def index
     render json: restaurant, only: [:id, :name, :address]
 end
 def show 
-    restaurant = Restaurant.find(params[:id])
-    render json: {
-      id: restaurant.id,
-      name: restaurant.name,
-      address: restaurant.address,
-      pizzas: 
-        {
-          id: pizza.id,
-          name: pizza.name,
-          ingredients: pizza.ingredients
-        }
-   
-    }
+    restaurant = Restaurant.find_by(id: params[:id])
+    if restaurant 
+        render json: restaurant, only: [:id, :name, :address]
+    else
+        render json: {error: "Restaurant not found"}
+    end
 end
-
 
 end
